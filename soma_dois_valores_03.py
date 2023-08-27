24fee27f9a3230db74bf0537ab013a60cd2185bc
caedufreitas <Ti.carlosfreitas@outlook.com>
2023-08-27 12:48:39 -0300

    soma dois valores

---
+++ soma_dois_valores_03.py
@@ -0,0 +1,6 @@
+'''entre no sistema com dois valores e saia com a soma entre eles'''
+v1 = int(input('Digite o primeiro valor: '))
+v2 = int(input('Digite o segundo valor: '))
+print('A soma de {} + {} = {} '.format(v1, v2, v1 + v2))
+print('Acabou!')
+
